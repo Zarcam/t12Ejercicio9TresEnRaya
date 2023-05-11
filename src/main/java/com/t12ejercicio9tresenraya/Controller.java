c44f5728b3c74b4451e88dd4ab1d33d6a3b11eae
Zarcam <alejandrosatan.2896@gmail.com>
2023-05-11 21:11:06 +0200

    Reset y Start implementados

--- src/main/java/com/t12ejercicio9tresenraya/Controller.java
+++ src/main/java/com/t12ejercicio9tresenraya/Controller.java
@@ -1,12 +1,19 @@
 package com.t12ejercicio9tresenraya;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     @FXML
     private TextField fieldPlayer1;
     @FXML
@@ -15,9 +22,36 @@ public class Controller {
     private Button botonReset;
     @FXML
     private Button botonStart;
+    @FXML
+    private GridPane panelBotones;
 
     @FXML
     private void pulsadoBoton(ActionEvent event){
+        Button boton = (Button) event.getSource();
 
+        boton.setText("X");
+        boton.setDisable(true);
+    }
+
+    private void limpiarTablero(){
+        for(Node boton : panelBotones.getChildren()){
+            boton.setDisable(false);
+            if(boton instanceof Button){
+                ((Button) boton).setText("");
+            }
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        botonStart.setOnAction(event -> {
+            limpiarTablero();
+        });
+
+        botonReset.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
+            limpiarTablero();
+            fieldPlayer1.setText("0");
+            fieldPlayer2.setText("0");
+        });
     }
 }
